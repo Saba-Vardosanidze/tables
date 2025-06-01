1edb963415da2f14a19c97c84e04decede7c3621
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-06-01 19:25:50 +0400

    update

--- features/table/components/Table.tsx
+++ features/table/components/Table.tsx
@@ -16,17 +16,14 @@ const columHelper = createColumnHelper<User>();
 
 const Table = ({initialData}: {initialData: User[]}) => {
   const [data, setData] = useState<User[]>(initialData);
-
   const [sorting, setSortingt] = useState<SortingState>([]);
 
   const table = useReactTable<User>({
     data: data,
     columns: colums,
-
     state: {
       sorting,
     },
-
     initialState: {
       pagination: {
         pageSize: 8,
@@ -37,20 +34,20 @@ const Table = ({initialData}: {initialData: User[]}) => {
   });
 
   return (
-    <div className="bg-[#121318] m-auto mt-[48px] p-[40px] rounded-[10px] w-full max-w-[792px] min-h-[658px]">
-      <div className="flex justify-between items-center mb-[24px]">
-        <p className="font-medium text-[#FFFFFF] text-[23px]">ტრანზაქციები</p>
-        <div className="flex justify-between w-full max-w-[385px]">
-          <button className="flex items-center gap-[10px] px-[10px] border border-[#9BA0B820] rounded-[5px] w-full max-w-[315px] min-h-[50px] cursor-pointer">
-            <Filter width={24} height={24} />
-            <p className="text-[#9295A6] text-[14px]">ფილტრი</p>
-          </button>
-          <button className="flex justify-center items-center border border-[#9BA0B820] rounded-[5px] w-full max-w-[50px] min-h-[50px] cursor-pointer">
-            <Download width={24} height={24} />
-          </button>
-        </div>
-      </div>
+    <div className="flex flex-col justify-between bg-[#121318] m-auto mt-[48px] p-[40px] rounded-[10px] w-full max-w-[792px] min-h-[658px]">
       <div>
+        <div className="flex justify-between items-center mb-[24px]">
+          <p className="font-medium text-[#FFFFFF] text-[23px]">ტრანზაქციები</p>
+          <div className="flex justify-between w-full max-w-[385px]">
+            <button className="flex items-center gap-[10px] px-[10px] border border-[#9BA0B820] rounded-[5px] w-full max-w-[315px] min-h-[50px] cursor-pointer">
+              <Filter width={24} height={24} />
+              <p className="text-[#9295A6] text-[14px]">ფილტრი</p>
+            </button>
+            <button className="flex justify-center items-center border border-[#9BA0B820] rounded-[5px] w-full max-w-[50px] min-h-[50px] cursor-pointer">
+              <Download width={24} height={24} />
+            </button>
+          </div>
+        </div>
         <table className="w-full">
           <thead>
             {table.getHeaderGroups().map((headerGroup) => (
@@ -83,29 +80,31 @@ const Table = ({initialData}: {initialData: User[]}) => {
             ))}
           </tbody>
         </table>
-        <div className="flex sm:flex-row flex-col justify-between items-center mt-4 text-gray-700 text-sm">
-          <div className="flex items-center gap-[20px]">
-            <button
-              className="flex justify-center items-center bg-[#0B0F16] disabled:bg-transparent border border-[#D59A04] disabled:border-none rounded-[10px] w-[30px] h-[30px] cursor-pointer"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              <PrevButton />
-            </button>
-            <span className="flex items-center">
-              <div className="text-white text-sm">
-                {table.getState().pagination.pageIndex + 1}
-              </div>
-              <span className="ml-1"> ..... {table.getPageCount()}</span>
-            </span>
-            <button
-              className="flex justify-center items-center bg-[#0B0F16] disabled:bg-transparent border border-[#D59A04] disabled:border-none rounded-[10px] w-[30px] h-[30px] cursor-pointer"
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-            >
-              <NextButton />
-            </button>
-          </div>
+      </div>
+
+      <div className="flex flex-row justify-between items-center mt-4 text-gray-700">
+        <div></div>
+        <div className="flex items-center gap-[20px]">
+          <button
+            className="flex justify-center items-center bg-[#0B0F16] disabled:bg-transparent border border-[#D59A04] disabled:border-none rounded-[10px] w-[30px] h-[30px] cursor-pointer"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <PrevButton />
+          </button>
+          <span className="flex items-center">
+            <div className="text-white">
+              {table.getState().pagination.pageIndex + 1}
+            </div>
+            <span> ..... {table.getPageCount()}</span>
+          </span>
+          <button
+            className="flex justify-center items-center bg-[#0B0F16] disabled:bg-transparent border border-[#D59A04] disabled:border-none rounded-[10px] w-[30px] h-[30px] cursor-pointer"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <NextButton />
+          </button>
         </div>
       </div>
     </div>
